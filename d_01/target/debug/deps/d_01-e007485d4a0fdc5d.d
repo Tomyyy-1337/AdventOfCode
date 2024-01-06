91d6df0dfccc01596365faaac89bac795966a2b6
Tom Paßberg <passberg@rhrk.uni-kl.de>
2024-01-06 21:43:48 +0100

    erste aufgabe

---
+++ d_01/target/debug/deps/d_01-e007485d4a0fdc5d.d
@@ -0,0 +1,5 @@
+c:\Users\tompa\Documents\Rust\AdventOfCode\d_01\target\debug\deps\libd_01-e007485d4a0fdc5d.rmeta: src\main.rs
+
+c:\Users\tompa\Documents\Rust\AdventOfCode\d_01\target\debug\deps\d_01-e007485d4a0fdc5d.d: src\main.rs
+
+src\main.rs:
---
+++ d_01/target/release/d_01.d
@@ -0,0 +1,1 @@
+C:\Users\tompa\Documents\Rust\AdventOfCode\d_01\target\release\d_01.exe: C:\Users\tompa\Documents\Rust\AdventOfCode\d_01\src\main.rs

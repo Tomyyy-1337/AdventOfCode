c96629c8b01bb3bceeb38e3182bd45d377757b57
Tom Paßberg <passberg@rhrk.uni-kl.de>
2024-01-08 17:17:16 +0100

    day3b

--- day03b/src/main.rs
+++ day03b/src/main.rs
@@ -18,24 +18,28 @@ impl Schematic {
         &self.components[(y * self.width + x) as usize]
     }
 
-    fn get_neighbour(&self, x: u32, y: u32) -> &Component {
-        for (dx, dy) in &OFFSETS {
-            let nx = x as i32 + dx;
-            let ny = y as i32 + dy;
-            if (0..self.width as i32).contains(&nx) && (0..self.height as i32).contains(&ny) {
-                let component = self.get_component(nx as u32, ny as u32);
-                if let &Component::Digit(_) = component {
-                    continue;
-                }
-                if component != &Component::Empty {
-                    return component;
-                }
-            }
-        }
-        &Component::Empty
+    pub fn gear_ratios(&self) -> Vec<(u32, u32)> {
+        (0..self.width*self.height)
+            .filter_map(|i| self.gear_ratio_at(i % self.width, i / self.width))
+            .collect()
     }
 
-    fn get_number_at(&self, x: u32, y: u32) -> Option<u32> {
+    fn gear_ratio_at(&self, x: u32, y: u32) -> Option<(u32,u32)> {
+        if let Component::Symbol('*') = self.get_component(x, y) {
+            let numbers = OFFSETS.iter()
+                .filter_map(|(dx, dy)| self.get_number_at((x as i32 + dx) as u32, ((y as i32) + dy) as u32))
+                .collect::<std::collections::HashSet<_>>()
+                .into_iter()
+                .map(|(a, _)| a)
+                .collect::<Vec<_>>();
+            if numbers.len() == 2 {
+                return Some((numbers[0], numbers[1]))
+            }
+        }
+        None
+    }
+
+    fn get_number_at(&self, x: u32, y: u32) -> Option<(u32, u32)> {
         if let Component::Digit(_) = self.get_component(x, y) {
             let mut start = x as i32;
             while let Component::Digit(_) = self.get_component(start as u32, y) {
@@ -45,6 +49,7 @@ impl Schematic {
                 }
             }
             start += 1;
+            let start_pos = start as u32;
             let mut num = 0;
             while let Component::Digit(digit) = self.get_component(start as u32, y) {
                 num = num * 10 + digit;
@@ -53,31 +58,9 @@ impl Schematic {
                     break;
                 }
             }
-            return Some(num);
+            return Some((num,start_pos as u32));
         }
         None
-    }
-
-    fn get_numbers(&self) -> Vec<u32> {
-        let mut numbers = Vec::new();
-        let mut skip = false;
-        for y in 0..self.height {
-            for x in 0..self.width {
-                match self.get_component(x, y) {
-                    Component::Digit(_) => {
-                        if let Some(num) = self.get_number_at(x, y) {
-                            if !skip && self.get_neighbour(x, y) != &Component::Empty {
-                                numbers.push(num);
-                                skip = true;
-                            }
-                        }
-                    }
-                    _ => skip = false,
-                }
-            }
-            skip = false;
-        }
-        numbers
     }
 }
 
@@ -94,8 +77,9 @@ fn main() {
                 '\r' | '\n' => None,
                 c => Some(Component::Symbol(c)),
             }).collect(),
-    }.get_numbers()
+    }.gear_ratios()
     .iter()
+    .map(|(a, b)| a * b)
     .sum::<u32>();
 
     println!("Summe = {}", sum);

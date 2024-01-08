b4cef7ad56558c9fb54a5f28358529faa380eea0
Tom Paßberg <passberg@rhrk.uni-kl.de>
2024-01-08 17:39:47 +0100

    -

--- day03b/src/main.rs
+++ day03b/src/main.rs
@@ -4,7 +4,6 @@ const OFFSETS:[(i32,i32); 8] = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (1, 
 enum Component {
     Digit(u32),
     Symbol(char),
-    Empty,
 }
 
 struct Schematic {
@@ -14,26 +13,40 @@ struct Schematic {
 }
 
 impl Schematic {
+    pub fn new(str: &str) -> Self {
+        Self {
+            width: str.lines().next().unwrap().len() as u32,
+            height: str.lines().count() as u32,
+            components: str.chars()
+                .filter_map(|c| match c {
+                    '\r' | '\n' => None,
+                    '0'..='9' => Some(Component::Digit(c as u32 - '0' as u32)),
+                    c => Some(Component::Symbol(c))})
+                .collect(),
+        }
+    }
+
     fn get_component(&self, x: u32, y: u32) -> &Component {
         &self.components[(y * self.width + x) as usize]
     }
 
-    pub fn gear_ratios(&self) -> Vec<(u32, u32)> {
+    pub fn gear_ratios(&self) -> Vec<u32> {
         (0..self.width*self.height)
             .filter_map(|i| self.gear_ratio_at(i % self.width, i / self.width))
             .collect()
     }
 
-    fn gear_ratio_at(&self, x: u32, y: u32) -> Option<(u32,u32)> {
+    fn gear_ratio_at(&self, x: u32, y: u32) -> Option<u32> {
         if let Component::Symbol('*') = self.get_component(x, y) {
             let numbers = OFFSETS.iter()
-                .filter_map(|(dx, dy)| self.get_number_at((x as i32 + dx) as u32, ((y as i32) + dy) as u32))
+                .map(|(dx, yd)| ((x as i32 + dx) as u32, (y as i32 + yd) as u32))
+                .filter_map(|(x, y)| self.get_number_at(x,y))
                 .collect::<std::collections::HashSet<_>>()
                 .into_iter()
                 .map(|(a, _)| a)
                 .collect::<Vec<_>>();
             if numbers.len() == 2 {
-                return Some((numbers[0], numbers[1]))
+                return Some(numbers[0] * numbers[1])
             }
         }
         None
@@ -65,22 +78,11 @@ impl Schematic {
 }
 
 fn main() {
-    let path = "input/test.txt";
-    let contents = std::fs::read_to_string(path).expect("Failed to read file");
-    let sum = Schematic {
-        width: contents.lines().next().unwrap().len() as u32,
-        height: contents.lines().count() as u32,
-        components: contents.chars()
-            .filter_map(|c| match c {
-                '0'..='9' => Some(Component::Digit(c as u32 - '0' as u32)),
-                '.' => Some(Component::Empty),
-                '\r' | '\n' => None,
-                c => Some(Component::Symbol(c)),
-            }).collect(),
-    }.gear_ratios()
-    .iter()
-    .map(|(a, b)| a * b)
-    .sum::<u32>();
+    let path = "input/puzzle.txt";
+    let sum = Schematic::new(&std::fs::read_to_string(path).unwrap())
+        .gear_ratios()
+        .iter()
+        .sum::<u32>();
 
     println!("Summe = {}", sum);
 }

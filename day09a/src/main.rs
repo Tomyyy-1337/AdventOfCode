1db23b2e82caf8e820caeadd8dd1c14b47ce9443
Tom Paßberg <passberg@rhrk.uni-kl.de>
2024-01-11 14:57:54 +0100

    day 9

---
+++ day09a/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    let path = "input/puzzle.txt";
+    let sum = std::fs::read_to_string(path).unwrap()
+        .lines()
+        .map(|line| line.split_ascii_whitespace().map(|str| str.parse::<i64>().unwrap()).collect::<Vec<_>>())
+        .map(|line| find_next_number(line))
+        .sum::<i64>();
+
+    println!("Sum: {}", sum);
+}
+
+fn find_next_number(seqence: Vec<i64>) -> i64 {
+    let arr: Vec<i64> = seqence.windows(2).map(|w| w[1] - w[0]).collect();
+    if arr.iter().find(|&x| x != &seqence[0]).is_none() {
+        return seqence.last().unwrap() + arr[0];
+    }
+    return seqence.last().unwrap() + find_next_number(arr);
+}

5fff89c94755444966c885cd126f25d0f273f407
Tom Paßberg <passberg@rhrk.uni-kl.de>
2024-01-17 18:12:52 +0100

    Day 19

--- day19/part2/src/main.rs
+++ day19/part2/src/main.rs
@@ -42,7 +42,7 @@ enum Operator {
 
 impl Operator {
     fn from_str(s: &str) -> Operator {
-        if s.split(':').count() == 1 {
+        if !s.contains(':') {
             return Operator::Const(Tag::from_str(s));
         }
         let mut input = s.split(':');
@@ -58,7 +58,7 @@ impl Operator {
 
     fn evaluate_range(&self, range: &Range) -> Option<(Tag, Range, Range)> {
         match self {
-            Operator::Const(tag) => Some((tag.clone(), range.clone(), Range{ ranges: vec![(0, 0), (0, 0), (0, 0), (0, 0)] })),
+            Operator::Const(tag) => Some((tag.clone(), range.clone(), Range::new(0,0))),
             Operator::LessThan (category, value, tag) if range.get_value(category).0 < *value => {
                 let range_hit = range.set_value(category, (range.get_value(category).0, *value));
                 let range_miss = range.set_value(category, (*value, range.get_value(category).1));
@@ -96,6 +96,10 @@ struct Range {
 }
 
 impl Range { 
+    fn new(lower: i32, upper: i32) -> Range {
+        Range { ranges: vec![(lower, upper), (lower, upper), (lower, upper), (lower, upper)] }
+    }
+    
     fn get_value(&self, category: &Category) -> (i32, i32) {
         self.ranges[*category as usize]
     }
@@ -123,22 +127,20 @@ fn main() {
         .map(Workflow::from_str)
         .collect();
 
-    let mut ranges = Vec::new();
-    let mut stack = vec![(Tag::Named("in".to_string()), Range{ ranges: vec![(1, 4001), (1, 4001), (1, 4001), (1, 4001)] })];
+    let mut count = 0;
+    let mut stack = vec![(Tag::Named("in".to_string()), Range::new(1,4001))];
     while let Some((tag, mut range)) = stack.pop() {
-        let workflow = workflow_map.get(&tag).unwrap();
-        for operator in workflow.filter.iter() {
+        for operator in workflow_map.get(&tag).unwrap().filter.iter() {
             if let Some((tag, range_hit, range_miss)) = operator.evaluate_range(&range) {
                 match tag {
                     Tag::Named(_) => stack.push((tag, range_hit)),
-                    Tag::Accept => ranges.push(range_hit),
+                    Tag::Accept => count += range_hit.abs(),
                     Tag::Reject => (),
                 }
                 range = range_miss;
             } 
         }
     }
-    let sum = ranges.iter().map(|r| r.abs()).sum::<u64>();
     
-    println!("Sum: {}", sum);
+    println!("Sum: {}", count);
 }

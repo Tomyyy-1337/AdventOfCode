10c329b743eb8f4a169092d0cb774e03fd9ffa30
Tom Paßberg <passberg@rhrk.uni-kl.de>
2024-02-17 14:17:47 +0100

    Day 24 part 1

--- day24/part1/src/main.rs
+++ day24/part1/src/main.rs
@@ -1,6 +1,6 @@
 use nalgebra as na;
+use rayon::iter::{IndexedParallelIterator, IntoParallelRefIterator, ParallelIterator};
 
-#[derive(Debug)]
 struct Vec2 {
     x: i64,
     y: i64,
@@ -16,7 +16,6 @@ impl Vec2 {
     }
 }
 
-#[derive(Debug)]
 struct Particle {
     position: Vec2,
     velocity: Vec2,
@@ -40,25 +39,17 @@ fn main() {
         .map(Particle::from_str)
         .collect();
 
-    let mut collisions = 0;
-
-    for i in 0..particles.len() {
-        for j in i+1..particles.len() {
-            let p1 = &particles[i];
-            let p2 = &particles[j];
-
-            let a = na::DMatrix::from_row_slice(2, 2, &[p1.velocity.x as f64, -p2.velocity.x as f64, p1.velocity.y as f64, -p2.velocity.y as f64]);
-            let b = na::DVector::from_row_slice(&[p2.position.x as f64 - p1.position.x as f64, p2.position.y as f64 - p1.position.y as f64]);
-            
-            if let Some(val) = a.lu().solve(&b) {
+    let collisions: usize = particles.par_iter().enumerate().map(|(i, p1)| {
+        particles.iter().skip(i+1).filter(|p2| {
+            let a: na::Matrix<f64,_,_,_> = na::DMatrix::from_row_slice(2, 2, &[p1.velocity.x as f64, -p2.velocity.x as f64, p1.velocity.y as f64, -p2.velocity.y as f64]);
+            let b: na::Matrix<f64,_,_,_> = na::DVector::from_row_slice(&[p2.position.x as f64 - p1.position.x as f64, p2.position.y as f64 - p1.position.y as f64]);
+            a.lu().solve(&b).is_some_and(| val | {
                 let x = val[0] * p1.velocity.x as f64 + p1.position.x as f64;
                 let y = val[0] * p1.velocity.y as f64 + p1.position.y as f64;
-                if BOUND.contains(&x) && BOUND.contains(&y) && val[0] >= 0.0 && val[1] >= 0.0 {
-                    collisions += 1;
-                }
-            }
-        }
-    }
+                BOUND.contains(&x) && BOUND.contains(&y) && val[0] >= 0.0 && val[1] >= 0.0 
+            }) 
+        }).count()
+    }).sum();
 
     println!("Collisions: {:?}", collisions)
-}
+} 

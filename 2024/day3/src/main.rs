9f8f827136fbda166e1e9fac73c8966627537d00
Tom Paßberg <tom.passberg@hotmail.com>
2024-12-04 01:18:04 +0100

    day3 2

--- 2024/day3/src/main.rs
+++ 2024/day3/src/main.rs
@@ -3,8 +3,16 @@ use regex::Regex;
 fn main() {
     let contents = std::fs::read_to_string("input/puzzle").unwrap();
 
+    let start = std::time::Instant::now();
     part1(&contents);
+    let time_part1 = start.elapsed();
+    let start = std::time::Instant::now();
+    part_1_alt(&contents);
+    let time_part1_alt = start.elapsed();
     part2(&contents);
+
+    println!("Time: {}µs", time_part1.as_micros());
+    println!("Time: {}µs", time_part1_alt.as_micros());
 }
 
 fn part1(contents: &str) {
@@ -23,62 +31,40 @@ fn part1(contents: &str) {
 
 fn part2(contents: &str) {
     let re_mult = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)").unwrap();
-    let re_do = Regex::new(r"do\(\)").unwrap(); 
-    let re_dont = Regex::new(r"don't\(\)").unwrap();
+    let re = Regex::new(r"(mul\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don't\(\))").unwrap();
 
-    let mults = re_mult.captures_iter(contents)
-        .map(|cap| {
-            let start_index = cap.get(0).unwrap().start();
-            let v1: u32 = cap[1].parse().unwrap();
-            let v2: u32 = cap[2].parse().unwrap();
-            (start_index, v1 * v2)
-        })
-        .collect::<Vec<(usize, u32)>>();
-
-    let mut dos = re_do.captures_iter(contents)
-        .map(|cap| {
-            let start_index: usize = cap.get(0).unwrap().start();
-            start_index
-        })
-        .peekable();
-
-    let mut donts = re_dont.captures_iter(contents)
-        .map(|cap| {
-            let start_index = cap.get(0).unwrap().start();
-            start_index
-        })
-        .peekable();
-
-    let mut active_instructions = Vec::new();   
-    while let (Some(doo), Some(dont)) = (dos.peek(), donts.peek()) {
-        if doo < dont {
-            active_instructions.push((*doo, true));
-            dos.next();
-        } else {
-            active_instructions.push((*dont, false));
-            donts.next();
-        }
-    }
-    dos.for_each(|doo| active_instructions.push((doo, true)));
-    donts.for_each(|dont| active_instructions.push((dont, false)));
-
-    let mut active_instructions = active_instructions.iter().peekable();
-
-    let mut result = 0;
-    let mut active = true;
-    for (i, mult) in mults.iter() {
-        while let Some((index, is_do)) = active_instructions.peek() {
-            if *index < *i {
-                active = *is_do;
-                active_instructions.next();
-            } else {
-                break;
+    let result = re.find_iter(contents)
+        .fold((0, true), |(sum, active), m| match m.as_str() {
+            "do()" => (sum, true),
+            "don't()" => (sum, false),
+            m if active => {
+                let cap = re_mult.captures(m).unwrap();
+                let a: i32 = cap[1].parse().unwrap();
+                let b: i32 = cap[2].parse().unwrap();
+                (sum + a * b, active)
             }
-        }
-        if active {
-            result += mult;
-        }
-    }
+            _ => (sum, active)
+        }).0;
 
-    println!("Part_2 Result: {}", result);
+    println!("Part_2 Result: {}", result); 
+}
+
+fn part_1_alt(contents: &str) {
+    let chars = contents.as_bytes();
+
+    let result = chars.windows(12)
+        .filter_map(|w| {
+            w.strip_prefix(b"mult(")
+                .and_then(|w| w.strip_suffix(b")"))
+                .map(|w| w.split(|&c| c == b','))
+        })
+        .map(|mut it| (it.next().unwrap(), it.next().unwrap()))
+        .map(|(a, b)| {
+            let a = a.iter().rev().fold(0, |acc, &c| acc * 10 + (c - b'0') as i32);
+            let b = b.iter().rev().fold(0, |acc, &c| acc * 10 + (c - b'0') as i32);
+            a * b
+        })
+        .sum::<i32>();
+
+    println!("Part_1 Result: {}", result);
 }

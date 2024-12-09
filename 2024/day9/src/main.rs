8c2e39b586f3ae176858a76c5f260a627dc7e602
Tom Paßberg <tom.passberg@hotmail.com>
2024-12-09 18:38:29 +0100

    day 9

--- 2024/day9/src/main.rs
+++ 2024/day9/src/main.rs
@@ -1,82 +1,76 @@
-use std::fmt::Debug;
+use std::{fmt::Debug, iter};
 
 fn main() {
     let contents = include_str!("../input/puzzle");
     
     part_1(contents);
     
-    let start = std::time::Instant::now();
     part_2(contents);
-    println!("Time: {:?}", start.elapsed());
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum BlockType {
+    Empty,
+    File{ id: u16 },
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 struct Block {
-    block_type: FileType,
+    block_type: BlockType,
     length: u8,
+}
+
+impl Block {
+    fn empty(length: u8) -> Self {
+        Self {
+            block_type: BlockType::Empty,
+            length,
+        }
+    }
+
+    fn file(id: u16, length: u8) -> Self {
+        Self {
+            block_type: BlockType::File { id },
+            length,
+        }
+    }
 }
 
 fn part_2(contents: &str) {
     let mut filesystem = contents
-    .char_indices()
+        .char_indices()
         .map(|(i, c)| {
             match i % 2 {
-                0 => Block {
-                    block_type: FileType::File { id: i as u16 / 2 },
-                    length: c.to_digit(10).unwrap() as u8,
-                }, 
-                1 => Block {
-                    block_type: FileType::Empty,
-                    length: c.to_digit(10).unwrap() as u8,
-                },
+                0 => Block::file(i as u16 / 2, c.to_digit(10).unwrap() as u8),
+                1 => Block::empty(c.to_digit(10).unwrap() as u8),
                 _ => unreachable!(),
             }
         })
         .collect::<Vec<_>>();
-    
-    let mut extract_index = filesystem
-        .iter()
-        .enumerate()
-        .rev()
-        .find_map(|(i, x)| if x.block_type != FileType::Empty { Some(i) } else { None })
-        .unwrap();
 
+    let mut extract_index = filesystem.len() - 1;
+    
     while extract_index > 0 {
         match filesystem[extract_index] {
-            Block { block_type: FileType::Empty, .. } => {
+            Block { block_type: BlockType::Empty, .. } => {
                 extract_index -= 1;
             } 
-            Block { block_type: FileType::File { id }, length } => {
+            Block { block_type: BlockType::File { id }, length } => {
                 let insert_index = filesystem[0..extract_index]
                     .iter()
-                    .position(|x| x.block_type == FileType::Empty && x.length >= length);
+                    .position(|x| x.block_type == BlockType::Empty && x.length >= length);
                 
                 match insert_index {
                     Some(insert_index) if filesystem[insert_index].length == length => {
-                        filesystem[insert_index] = Block {
-                            block_type: FileType::File { id },
-                            length,
-                        };
-                        filesystem[extract_index] = Block {
-                            block_type: FileType::Empty,
-                            length,
-                        };
+                        filesystem[insert_index] = Block::file(id, length);
+                        filesystem[extract_index] = Block::empty(length);
                         extract_index -= 2;
                     }
                     Some(insert_index) => {
                         let remaining = filesystem[insert_index].length - length;
-                        filesystem[insert_index] = Block {
-                            block_type: FileType::File { id },
-                            length,
-                        };
-                        filesystem[extract_index] = Block {
-                            block_type: FileType::Empty,
-                            length,
-                        };
-                        filesystem.insert(insert_index + 1, Block {
-                            block_type: FileType::Empty,
-                            length: remaining,
-                        });
+                        filesystem[insert_index] = Block::file(id, length);
+                        filesystem[extract_index] = Block::empty(length);
+                        filesystem.insert(insert_index + 1, Block::empty(remaining));
                         extract_index -= 1;
                     }
                     None => {
@@ -89,27 +83,15 @@ fn part_2(contents: &str) {
     
     let result = filesystem
         .iter()
-        .flat_map(|block| 
-            std::iter::repeat(block.block_type).take(block.length as usize)
-        )
+        .flat_map(|block| iter::repeat(block.block_type).take(block.length as usize))
         .enumerate()
         .filter_map(|(i, x)| match x {
-            FileType::File { id } => Some((i, id)),
-            _ => None,
+            BlockType::File { id } => Some(i as u64 * id as u64),
+            BlockType::Empty => None,
         })
-        .map(|(a,b)| a as u64 * b as u64)
         .sum::<u64>();
         
-        
-
     println!("Result: {}", result);
-}
-
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum FileType {
-    Empty,
-    File{ id: u16 },
 }
 
 fn part_1(contents: &str) {
@@ -117,8 +99,8 @@ fn part_1(contents: &str) {
         .char_indices()
         .flat_map(|(i, c)| {
             let file = match i % 2 {
-                0 => FileType::File { id: i as u16 / 2 },
-                1 =>  FileType::Empty,
+                0 => BlockType::File { id: i as u16 / 2 },
+                1 =>  BlockType::Empty,
                 _ => unreachable!(),
             };
             std::iter::repeat(file).take(c.to_digit(10).unwrap() as usize)
@@ -130,15 +112,15 @@ fn part_1(contents: &str) {
         .iter()
         .enumerate()
         .rev()
-        .find_map(|(i, &x)| if x != FileType::Empty { Some(i) } else { None })
+        .find_map(|(i, &x)| if x != BlockType::Empty { Some(i) } else { None })
         .unwrap();
 
     while insert_index < extract_index {
-        let offset = filesystem[insert_index..extract_index].iter().position(|&x| x == FileType::Empty).unwrap();
+        let offset = filesystem[insert_index..extract_index].iter().position(|&x| x == BlockType::Empty).unwrap();
         filesystem[insert_index + offset] = filesystem[extract_index];
-        filesystem[extract_index] = FileType::Empty;
+        filesystem[extract_index] = BlockType::Empty;
         insert_index += offset + 1;
-        while extract_index > 0 && filesystem[extract_index] == FileType::Empty {
+        while extract_index > 0 && filesystem[extract_index] == BlockType::Empty {
             extract_index -= 1;
         }
     }
@@ -147,7 +129,7 @@ fn part_1(contents: &str) {
         .iter()
         .enumerate()
         .filter_map(|(i, &x)| match x {
-            FileType::File { id } => Some((i, id)),
+            BlockType::File { id } => Some((i, id)),
             _ => None,
         })
         .map(|(a,b)| a as u64 * b as u64)
@@ -155,4 +137,3 @@ fn part_1(contents: &str) {
 
     println!("Result: {}", result);
 }
-
